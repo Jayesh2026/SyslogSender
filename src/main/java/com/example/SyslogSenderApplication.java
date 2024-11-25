d34132da05a1fb1d6fa6ddd43c83145ec591f3cd
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-11-25 12:39:19 +0530

    removed not required part

--- src/main/java/com/example/SyslogSenderApplication.java
+++ src/main/java/com/example/SyslogSenderApplication.java
@@ -6,22 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SyslogSenderApplication {
 
-	// private final SyslogSenderService syslogSenderService;
-
-
-	// public SyslogSenderApplication(SyslogSenderService syslogSenderService) {
-    //     this.syslogSenderService = syslogSenderService;
-    // }
-
     public static void main(String[] args) {
         SpringApplication.run(SyslogSenderApplication.class, args);
     }
-
-    // @Override
-    // public void run(String... args) throws Exception {
-    //     // Send a test syslog message on startup
-    //     syslogSenderService.sendTestSyslogMessage();
-    // }
-
 
 }
